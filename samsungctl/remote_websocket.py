d11386d5c0090a7c998048903ec5f8358cf6e379
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2018-11-24 12:32:49 -0700

    Changes the timeout of the websocket connection
    This changes the timeout of the websocket connection to 10 seconds if there is no token stored for a connection.
    I have also added a print statement to print the json output. I added a _read_response() to the end of the contol method and removed the creating of an exception from the _read_response method if the data is not a connect data. this way it will also print out data for key presses

--- samsungctl/remote_websocket.py
+++ samsungctl/remote_websocket.py
@@ -11,12 +11,16 @@ from . import exceptions
 URL_FORMAT = "ws://{}:{}/api/v2/channels/samsung.remote.control?name={}"
 SSL_URL_FORMAT = "wss://{}:{}/api/v2/channels/samsung.remote.control?name={}"
 
+
 class RemoteWebsocket():
     """Object for remote control connection."""
 
     def __init__(self, config):
         import websocket
-        self.token_file = os.path.dirname(os.path.realpath(__file__)) + "/token.txt"
+
+
+        self.token_file = os.path.dirname(
+            os.path.realpath(__file__)) + "/token.txt"
         if not config["port"]:
             config["port"] = 8001
 
@@ -25,16 +29,29 @@ class RemoteWebsocket():
 
         if config["port"] == 8002:
             url = SSL_URL_FORMAT.format(config["host"], config["port"],
-                                        self._serialize_string(config["name"]))
+                self._serialize_string(config["name"]))
             if os.path.isfile(self.token_file):
                 with open(self.token_file, "r") as token_file:
-                    url += "&token=" + token_file.readline()
-            self.connection = websocket.create_connection(url, config["timeout"], sslopt={"cert_reqs": ssl.CERT_NONE})
+                    token = token_file.readline()
+                    if token.strip():
+                        url += "&token=" + token
+                        has_token = True
+                    else:
+                        has_token = False
+            else:
+                has_token = False
+
+            timeout = config["timeout"] if has_token else 10.0
+            self.connection = websocket.create_connection(url, timeout,
+                sslopt={"cert_reqs": ssl.CERT_NONE})
+
+            self.has_token = has_token
 
         else:
             url = URL_FORMAT.format(config["host"], config["port"],
-                                    self._serialize_string(config["name"]))
-            self.connection = websocket.create_connection(url, config["timeout"])
+                self._serialize_string(config["name"]))
+            self.connection = websocket.create_connection(url,
+                config["timeout"])
         self._read_response()
 
     def __enter__(self):
@@ -58,9 +75,9 @@ class RemoteWebsocket():
         payload = json.dumps({
             "method": "ms.remote.control",
             "params": {
-                "Cmd": "Click",
-                "DataOfCmd": key,
-                "Option": "false",
+                "Cmd":          "Click",
+                "DataOfCmd":    key,
+                "Option":       "false",
                 "TypeOfRemote": "SendRemoteKey"
             }
         })
@@ -69,21 +86,26 @@ class RemoteWebsocket():
         self.connection.send(payload)
         time.sleep(self._key_interval)
 
+        self._read_response()
+
     _key_interval = 0.5
 
     def _read_response(self):
         response = self.connection.recv()
         response = json.loads(response)
+        
+        print(json.dumps(response, indent=4))
 
         if 'data' in response and 'token' in response["data"]:
             with open(self.token_file, "w") as token_file:
+                self.has_token = True
                 token_file.write(response['data']["token"])
 
-        if response["event"] != "ms.channel.connect":
-            self.close()
-            raise exceptions.UnhandledResponse(response)
-
-        logging.debug("Access granted.")
+        # if response["event"] != "ms.channel.connect":
+        #     self.close()
+        #     raise exceptions.UnhandledResponse(response)
+        # 
+        # logging.debug("Access granted.")
 
     @staticmethod
     def _serialize_string(string):
